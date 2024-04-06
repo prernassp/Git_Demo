4bdc20a79b265b456c1f0d17d4a678abf862a6ca
Prerna <prernassp@gmail.com>
2024-04-06 18:47:47 +0530

    Added Python File

---
+++ p1.py
@@ -0,0 +1,11 @@
+g=int(input("Enter the Grade"))
+if(50<=g<=60):
+    print("Grade is C")
+elif(60<g<=70):
+    print("Grade is B")
+elif(70<g<=80):
+    print("Grade is A")
+elif(g>80):
+    print("Grade is A++")
+else:
+    print("Not worth to mention")
